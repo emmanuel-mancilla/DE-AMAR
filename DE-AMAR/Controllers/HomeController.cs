945f635f13a3e784b776324cd8ab6adf84dafe80
EMMANUEL AMERICO MANCILLA MUNOZ <emmanuel.mancilla@anmat.gob.ar>
2025-06-15 18:14:48 -0300

    Agregar archivos de proyecto.

---
+++ DE-AMAR/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+using System.Diagnostics;
+using DE_AMAR.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DE_AMAR.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult AMAR()
+        {
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DE-AMAR/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DE_AMAR.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
